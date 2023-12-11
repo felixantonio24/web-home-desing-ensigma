1740d30ea367f2fb242f05dfdba9d6d604791486
felix <luisfelixtv@outllok.es>
2023-12-10 23:38:35 -0500

    proces

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+const login = document.querySelector('#logins')
+const modal = document.querySelector("#modal")
+
+login.addEventListener("click", ()=> {
+    modal.classList.add(".modal--listener")
+})
