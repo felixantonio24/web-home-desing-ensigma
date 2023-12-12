392207173e555b1f78608aa8f441ae0c49581d6f
felix <luisfelixtv@outllok.es>
2023-12-11 21:30:30 -0500

    proecs3

--- assets/js/main.js
+++
@@ -1,6 +0,0 @@
-const login = document.querySelector('#logins')
-const modal = document.querySelector("#modal")
-
-login.addEventListener("click", ()=> {
-    modal.classList.add(".modal--listener")
-})
