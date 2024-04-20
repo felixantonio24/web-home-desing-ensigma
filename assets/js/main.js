3f0094f86c2ea9e54a9eacc125ce8d250954d7ed
felix <luisfelixtv@outlook.es>
2024-04-20 01:12:56 -0500

    proscss

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+const loginButton = document.querySelector("#logins");
+const modal = document.querySelector(".container_login");
+
+function abrirModal() {
+    modal.style.display = "block";
+    setTimeout(() => {
+        modal.style.opacity = "1";
+    }, 10); // Espera 10ms para asegurarse de que el cambio de display a block ocurra antes de cambiar la opacidad
+}
+
+loginButton.addEventListener("click", abrirModal);
