dd2c61f00015d10593c19170c5a75c599cea4fc4
felix <luisfelixtv@outlook.es>
2024-04-25 23:26:30 -0500

    procs-modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,10 @@
 
 const loginButton = document.querySelector("#logins");
 const modal = document.querySelector(".container_login");
-
+const image = document.querySelector(".img_header")
 function abrirModal() {
     modal.style.display = "block";
+    image.style.opacity= "0.2"
     setTimeout(() => {
         modal.style.opacity = "1";
     }, 10); // Espera 10ms para asegurarse de que el cambio de display a block ocurra antes de cambiar la opacidad
